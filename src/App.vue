f499aa1e3290470a166cbacd667744b3649cad39
joelmukanya <joel@lifechoices.co.za>
2022-07-09 21:55:40 +0200

    Added style sheet

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <UserInput />
+  <fieldset id="form-wrapper">
+    <legend>{{ heading }}</legend>
+    <form @submit.prevent="display">
+      <UserInput v-model="name" :placeholder="enterName"/>
+      <UserInput v-model="surname" :placeholder="enterSurname"/>
+      <label> {{ result }}</label>
+      <button>Submit</button>
+    </form>
+  </fieldset>
+
 </template>
 
 <script>
@@ -9,6 +18,21 @@ export default {
   name: 'App',
   components: {
     UserInput
+  },
+  data() {
+    return {
+      heading: 'Please fill in your details',
+      name: '',
+      surname: '',
+      enterName: 'Enter your name',
+      enterSurname: 'Enter your surname',
+      result: ''
+    }
+  },
+  methods: {
+    display() {
+      this.result = `${this.name} ${this.surname}`;
+    }
   }
 }
 </script>
--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,12 +1,26 @@
 <template>
-  <input/>
+  <input v-model="inputData"/>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-
+  props: [
+    'modelValue'
+  ],
+  computed: {
+    inputData: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        /*Emit an event that other components     will be able to listen to.
+        We can emit onclick event or a custom 
+        event a developer created
+        */
+       // Below syntax it only used in vue3
+       // $emit(type of event, value)
+        this.$emit('update:modelValue', value);
+      }
     }
   }
 }
